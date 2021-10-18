56c21094c459ee33bfb281d22f9741f437dfb89d
tithi23 <tithiratry@gmail.com>
2021-10-18 09:56:45 -0700

    react firebase authentication related problem email and password set related problem

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDqhg1eW-BKGfTcx6O7vnc8m5rsAtYNptc",
+    authDomain: "react-firebase-authentic-e1fdd.firebaseapp.com",
+    projectId: "react-firebase-authentic-e1fdd",
+    storageBucket: "react-firebase-authentic-e1fdd.appspot.com",
+    messagingSenderId: "169434100525",
+    appId: "1:169434100525:web:6c802ff953f8e3e8cc19a4"
+};
+
+export default firebaseConfig;
